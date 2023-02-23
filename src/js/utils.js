2b092200c1e5a8d4e8e20f96d47b11a9ea283f6a
Adrien Pauchet <grandchefdotkong@gmail.com>
2023-02-23 16:02:08 +0900

    fliter/sort posts added

--- src/js/utils.js
+++ src/js/utils.js
@@ -15,3 +15,41 @@ export function formatDate(date) {
   })
 }
 
+export function formatBlogPosts(posts, {
+  filterOutDrafts = true,
+  filterOutFuturPosts = true,
+  sortByDate = true,
+  limit = undefined,
+
+} = {}) {
+
+  const filterPosts = posts.reduce((acc, post) => {
+    const { date, draft, status } = post.frontmatter;
+
+    if(filterOutDrafts && draft) {
+      return acc;
+    }
+
+    if(filterOutFuturPosts && new Date(date) > new Date) {
+      return acc;
+    }
+
+    acc.push(post);
+    
+    return acc;
+  }, [])
+
+  if(sortByDate) {
+    filterPosts.sort((a, b) => 
+      new Date(b.frontmatter.date) - new Date(a.frontmatter.date))
+  } else {
+    filterPosts.sort(() => Math.random() - 0.5) // randomize
+  }
+
+  if(typeof(limit) === "number") {
+    return filterPosts.slice(0, limit);
+  }
+
+  return filterPosts
+
+}
